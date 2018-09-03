9d0a1b75bdb67808c9167b3ba33366ed3f33d329
Chris <christopherscott9456@gmail.com>
2018-09-03 14:23:45 -0400

    Configured /user/register route to add new user to MongoDB table

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var jwt = require('jsonwebtoken');
+
+var User = require('../models/users');
 
 // Register
-router.get('/register', function(req, res, next) {
-	res.send('REGISTER');
+router.post('/register', function(req, res, next) {
+	let newUser = new User({
+		name: req.body.name,
+		email: req.body.email,
+		username: req.body.username,
+		password: req.body.password
+	});
+
+	User.addUser(newUser, function(err, user) {
+		if(err) {
+			res.json({success: false, msg: 'Failed to register user'});
+		}else {
+			res.json({success: true, msg: 'User successfully registered'})
+		}
+	});
 });
 
 router.post('/auth', function(req, res, next) {
@@ -13,11 +30,6 @@ router.post('/auth', function(req, res, next) {
 
 router.get('/profile', function(req, res, next) {
 	res.send('PROFILE');
-});
-
-
-router.get('/validate', function(req, res, next) {
-	res.send('VALIDATE');
 });
 
 module.exports = router;
